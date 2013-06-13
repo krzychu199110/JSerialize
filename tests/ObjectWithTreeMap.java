6891dd8a0ff969a14b844af9fb073e3717bc0aa5
Maciej Bialas <matej9991@gmail.com>
2013-06-13 23:33:50 +0200

    Klasa pomocnicza dla testu testObjectWithTreeMap

---
+++ tests/ObjectWithTreeMap.java
@@ -0,0 +1,16 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ObjectWithTreeMap {
+
+	Map TreeMap = new TreeMap<String,Object>();
+
+	public ObjectWithTreeMap() {
+		TreeMap.put("Karol", 11);
+        TreeMap.put("Marek", 12);
+	}
+}
+
