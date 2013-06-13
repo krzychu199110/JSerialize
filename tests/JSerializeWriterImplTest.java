e86f39b0c9c96c51bb6d4d5595b34fb892b8fe06
Maciej Bialas <matej9991@gmail.com>
2013-06-14 01:20:50 +0200

    Dodatkowe 2 testy dla klasy JSerializeWriterImpl

--- tests/JSerializeWriterImplTest.java
+++ tests/JSerializeWriterImplTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,6 +20,8 @@ public class JSerializeWriterImplTest {
 	ObjectWithTreeMap os5;
 	ObjectWithTransientField os6;
 	ObjectWithHashSet os7;
+	ObjectWithTreeSet os8;
+	ObjectWithLinkedList os9;
 
 	boolean equalMaps(Map<String, Object> m1, Map<String, Object> m2) {
 
@@ -37,6 +41,22 @@ public class JSerializeWriterImplTest {
 		os5 = new ObjectWithTreeMap();
 		os6 = new ObjectWithTransientField();
 		os7 = new ObjectWithHashSet();
+		os8 = new ObjectWithTreeSet();
+		os9 = new ObjectWithLinkedList();
+	}
+
+	@Test
+	public void testObjectWithPrimitives() {
+
+		Map<String, Object> expected = new HashMap<String, Object>();
+		expected.put("a#int", 10);
+		expected.put("b#int", 10);
+		expected.put("#JSerializeMetaData#RootClassName", os.getClass()
+				.getName());
+
+		Map<String, Object> generated = writer.prepareMap(os);
+
+		assertTrue(equalMaps(expected, generated));
 	}
 
 	@Test
@@ -60,6 +80,30 @@ public class JSerializeWriterImplTest {
 				.getName());
 
 		Map<String, Object> generated = writer.prepareMap(os7);
+
+		assertTrue(equalMaps(expected, generated));
+	}
+
+	@Test
+	public void testObjectWithTreeSet() {
+		Map<String, Object> expected = new HashMap<String, Object>();
+		expected.put("ts#java.util.TreeSet", new HashMap<String, Object>());
+		expected.put("#JSerializeMetaData#RootClassName", os8.getClass()
+				.getName());
+
+		Map<String, Object> generated = writer.prepareMap(os8);
+
+		assertTrue(equalMaps(expected, generated));
+	}
+
+	@Test
+	public void testObjectWithLinkedList() {
+		Map<String, Object> expected = new HashMap<String, Object>();
+		expected.put("link#java.util.LinkedList", new HashMap<String, Object>());
+		expected.put("#JSerializeMetaData#RootClassName", os9.getClass()
+				.getName());
+
+		Map<String, Object> generated = writer.prepareMap(os9);
 
 		assertTrue(equalMaps(expected, generated));
 	}
