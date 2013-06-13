4e29791320eb96480a3874852f920fa4ea216f56
Krzysztof Paszuk <krzychu199110@wp.pl>
2013-06-14 00:01:35 +0200

    Dodadnie Klas pomocniczych dla testow JModelImpl: Osoba,Rodzina,Test

---
+++ src/tests/Osoba.java
@@ -0,0 +1,16 @@
+package exesoft;
+
+public class Osoba {
+	public Osoba(String name, int age, int id) {
+		this.idOsoby = id;
+		this.name = name;
+		this.isDorosly = true;
+		this.age[0] = age;
+	}
+
+	int idOsoby;
+	boolean isDorosly;
+	String name;
+	int[] age = { 1, 2 };
+
+}
---
+++ src/tests/Rodzina.java
@@ -0,0 +1,8 @@
+package exesoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rodzina {
+	public List<Osoba> rodzina = new ArrayList<Osoba>();
+}
