acacea0564fd14f3f4404afdc359d8e1c318082f
Krzysztof Paszuk <krzychu199110@wp.pl>
2013-06-14 08:06:14 +0200

    Ponowne wrzucenie dodatkowych testow

--- src/tests/JModelImplTestE.java
+++ src/tests/JModelImplTestE.java
@@ -20,9 +20,9 @@ public class JModelImplTestE extends TestCase {
 	public void setUp() throws Exception {
 
 		testString = new String();
-		
+
 		Osoba tata = new Osoba("Karol", 30, 1);
-		Osoba mama = new Osoba("Jola",59,  2);
+		Osoba mama = new Osoba("Jola", 59, 2);
 
 		Rodzina rodzina = new Rodzina();
 
@@ -34,7 +34,7 @@ public class JModelImplTestE extends TestCase {
 
 		testString = jm.encode(js.prepareMap(rodzina));
 
-	}	
+	}
 
 	@Test
 	public void testJModelImplNotNull() throws Exception {
@@ -42,27 +42,6 @@ public class JModelImplTestE extends TestCase {
 		jModelImpl = new JModelImpl();
 
 		assertNotNull(jModelImpl);
-
-	}
-
-	@Test
-	// parzystosc cudzyslowów
-	public void testencodePairOfQuotationMarks() throws Exception {
-
-		int x = 0, y = 0;
-
-		for (int i = 0; i < testString.length(); i++) {
-			if (testString.charAt(i) == (char) 34)
-				x++;
-		}
-
-		for (int i = 0; i < testString.length(); i++) {
-			if (testString.charAt(i) == (char) 39)
-				y++;
-		}
-
-		assertEquals("Poprawnie utworzone cudzyslowy", 0, x % 2);
-		assertEquals("Poprawnie utworzone apostrofy", 0, y % 2);
 
 	}
 
@@ -77,7 +56,7 @@ public class JModelImplTestE extends TestCase {
 				break;
 			}
 		}
-		
+
 		assertEquals('{', testString.charAt(x + 1));
 
 	}
@@ -138,5 +117,77 @@ public class JModelImplTestE extends TestCase {
 		}
 
 		assertEquals(g, h);
+	}
+
+	@Test
+	// parzystosc cudzyslowów
+	public void testencodePairOfQuotationMarks() throws Exception {
+
+		int x = 0, y = 0;
+
+		for (int i = 0; i < testString.length(); i++) {
+			if (testString.charAt(i) == (char) 34)
+				x++;
+		}
+
+		for (int i = 0; i < testString.length(); i++) {
+			if (testString.charAt(i) == (char) 39)
+				y++;
+		}
+
+		assertEquals("Poprawnie utworzone cudzyslowy", 0, x % 2);
+		assertEquals("Poprawnie utworzone apostrofy", 0, y % 2);
+
+	}
+
+	@Test
+	// nawias otwarcia
+	public void testencodeCommaSpace() throws Exception {
+		int x = 0;
+		for (int i = 0; i < testString.length(); i++) {
+
+			if ((testString.charAt(i) == (char) 44)
+					&& (testString.charAt(i + 1) != (char) 32)) {
+				x = 1;
+				break;
+			}
+		}
+		assertEquals("Poprawna skadnia dot 'przecinek-spacja'", 0, x);
+	}
+
+	@Test
+	public void testencodeOpenBracketSpace() throws Exception {
+		int x = 0;
+		for (int i = 0; i < testString.length(); i++) {
+
+			if ((testString.charAt(i) == (char) 91)
+					&& (testString.charAt(i + 1) != (char) 32)) {
+				x = 1;
+				break;
+			}
+		}
+		assertEquals("Poprawna skadnia dot 'nawias kwadratowy-spacja'", 0, x);
+
+	}
+
+	@Test
+	public void testencodeQuotationMarksColon() throws Exception {
+		int x = 0;
+
+		for (int j = 0; j < testString.length(); j++) {
+
+			if ((testString.charAt(j) == (char) 58)
+					&& (testString.charAt(j - 1) != (char) 34)) {
+
+				if (testString.charAt(j - 1) != 'g') {
+					x = 1;
+					break;
+				}
+			}
+		}
+
+		System.out.println(testString);
+		assertEquals("Poprawnosc skadni dot String:value \": ", 0, x);
+
 	}
 }
